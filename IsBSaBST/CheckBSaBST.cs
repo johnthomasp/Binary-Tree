7fddb962a0e77234bdefdce6a0aa5d202d4226f7
John Thomas <johnthomasp@gmail.com>
2018-04-27 08:00:50 -0700

    Updated code to generate a binary search tree.

--- IsBSaBST/CheckBSaBST.cs
+++ IsBSaBST/CheckBSaBST.cs
@@ -54,73 +54,42 @@ namespace CheckBSaBST
         }
     }
 
-    public class CheckBST
+
+    public static class IsABST
     {
-        int count = 10;
-        public Node BuildaBSTree()
+        static int count = 10;
+        public static Node GenerateBST(int[] arr, int start, int end)
         {
-            //Tree Structure
-            //             10
-            //      -10           30
-            //          8      25    60
-            //        6   9      28    78 
-            Node rrr3 = new Node(78, null, null);
-            Node rr3 = new Node(28, null, null);
-            Node rr2 = new Node(60, null, rrr3);
-            Node rl2 = new Node(25, null, rr3);
-            Node r1 = new Node(30, rl2, rr2);
-            Node r3 = new Node(9, null, null);
-            Node l3 = new Node(6, null, null);
-            Node r2 = new Node(8, l3, r3);
-            Node l1 = new Node(-10, null, r2);
-            Node root = new Node(10, l1, r1);
-            return root;
+            if (start > end)
+                return null;
+            int mid = (start + end) / 2;
+            Node node = new Node(arr[mid]);
+            node.LeftChild = GenerateBST(arr, start, mid - 1);
+            node.RightChild = GenerateBST(arr, mid + 1, end);
+            return node;
         }
 
-
-        public Node BuildaNOTBSTree()
+        public static bool IsBst(Node bst)
         {
-            //Tree Structure
-            //             10
-            //      -10           30
-            //          8      25    60
-            //        6   200      28    78 
-            Node rrr3 = new Node(78, null, null);
-            Node rr3 = new Node(28, null, null);
-            Node rr2 = new Node(60, null, rrr3);
-            Node rl2 = new Node(25, null, rr3);
-            Node r1 = new Node(30, rl2, rr2);
-            Node r3 = new Node(200, null, null);
-            Node l3 = new Node(6, null, null);
-            Node r2 = new Node(8, l3, r3);
-            Node l1 = new Node(-10, null, r2);
-            Node root = new Node(10, l1, r1);
-            return root;
+            return IsBst(bst, int.MinValue, int.MaxValue);
         }
 
-        public bool IsBST(Node root)
+        public static bool IsBst(Node bst, int min, int max)
         {
-            return IsBST(root, int.MinValue, int.MaxValue);
-        }
-        
-
-        public bool IsBST(Node root, int min, int max)
-        {
-            if (root == null)
+            if (bst == null)
                 return true;
-            if ((root.Data <= min) || (root.Data > max))
+            if ((bst.Data < min) || (bst.Data > max))
                 return false;
-            
-            return (IsBST(root.LeftChild, min, root.Data) && IsBST(root.RightChild, root.Data, max));
+
+            return (IsBst(bst.LeftChild, min, bst.Data) && IsBst(bst.RightChild, bst.Data, max));
         }
 
-        public void printUtil(Node root, int space)
+        public static void printUtil(Node root, int space)
         {
             if (root == null)
                 return;
 
             space += count;
-
             // Process right child first
             printUtil(root.RightChild, space);
             Console.Write("\n");
@@ -134,7 +103,7 @@ namespace CheckBSaBST
             printUtil(root.LeftChild, space);
         }
 
-        public void Print(Node root)
+        public static void Print(Node root)
         {
             printUtil(root, 0);
         }
@@ -144,20 +113,30 @@ namespace CheckBSaBST
     {
         static void Main(string[] args)
         {
-            CheckBST checkBST = new CheckBST();
-            Node notBSTree = checkBST.BuildaNOTBSTree();
-            Node bsTree = checkBST.BuildaBSTree();
-            bool isBSaBSTree = false;
+            int size = 10;
+            int[] arr = new int[size];
+            Random random = new Random();
 
-            checkBST.Print(notBSTree);
-            isBSaBSTree = checkBST.IsBST(notBSTree);
-            Console.WriteLine("");
-            Console.WriteLine(isBSaBSTree ? "The current binary tree is a BST" : "The current binary tree is not a BST");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = random.Next(100);
+            }
 
-            checkBST.Print(bsTree);
-            isBSaBSTree = checkBST.IsBST(bsTree);
-            Console.WriteLine("");
-            Console.WriteLine(isBSaBSTree?"The current binary tree is a BST": "The current binary tree is not a BST");
+            int end = arr.Length - 1;
+            Node notbst = IsABST.GenerateBST(arr, 0, end);
+            Array.Sort(arr);
+            Node bst = IsABST.GenerateBST(arr, 0, end);
+
+            IsABST.Print(notbst);
+            Console.WriteLine("\n\n\n");
+            IsABST.Print(bst);
+
+            bool isBst = IsABST.IsBst(notbst);
+            Console.WriteLine(isBst ? "\n\nThe tree is a BST" : "\n\nThe tree is not a BST");
+
+            isBst = IsABST.IsBst(bst);
+            Console.WriteLine(isBst ? "\n\nThe tree is a BST" : "\n\nThe tree is not a BST");
+
             Console.ReadLine();
         }
     }
